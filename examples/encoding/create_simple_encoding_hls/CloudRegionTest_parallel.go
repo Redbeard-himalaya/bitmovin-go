d7c6756d2f5772b89f1ea265e446723fabef5036
redbeard <developer.bannon@gmail.com>
2021-08-23 20:56:55 +1200

    add a parallel test to create_simple_encoding_hls
    Signed-off-by: redbeard <developer.bannon@gmail.com>

---
+++ examples/encoding/create_simple_encoding_hls/CloudRegionTest_parallel.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/bitmovin/bitmovin-go/bitmovin"
+	"github.com/bitmovin/bitmovin-go/bitmovintypes"
+	"github.com/bitmovin/bitmovin-go/models"
+	"github.com/bitmovin/bitmovin-go/services"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	cloudRegions := []bitmovintypes.CloudRegion {
+		bitmovintypes.CloudRegionAWSUSEast1,
+		bitmovintypes.CloudRegionAWSUSWest1,
+		bitmovintypes.CloudRegionAWSUSWest2,
+		bitmovintypes.CloudRegionAWSEUWest1,
+		bitmovintypes.CloudRegionAWSEUCentral1,
+		bitmovintypes.CloudRegionAWSAPSouth1,
+		bitmovintypes.CloudRegionAWSAPNortheast1,
+		bitmovintypes.CloudRegionAWSAPNortheast2,
+		bitmovintypes.CloudRegionAWSAPSoutheast1,
+		bitmovintypes.CloudRegionAWSAPSoutheast2,
+		bitmovintypes.CloudRegionAWSSAEast1,
+		bitmovintypes.CloudRegionGoogleEuropeWest1,
+		bitmovintypes.CloudRegionGoogleUSEast1,
+		bitmovintypes.CloudRegionGoogleUSCentral1,
+		bitmovintypes.CloudRegionGoogleAsiaEast1,
+		bitmovintypes.CloudRegionExternal,
+	}
+
+	for _, cloudRegion := range cloudRegions {
+		wg.Add(1)
+		go CloudRegionTest(cloudRegion, wg)
+	}
+	wg.Wait()
+}
+
+func CloudRegionTest(cloudRegion bitmovintypes.CloudRegion, wg sync.WaitGroup) {
+	fmt.Printf("CloudRegionTest: %s\n", cloudRegion)
+	defer wg.Done()
+
+	// Creating Bitmovin object
+	bitmovin := bitmovin.NewBitmovin("2f44b2a4-3f93-49d9-9408-0b51c39df667", "https://api.bitmovin.com/v1/", 5)
+
+	// Creating the HTTP Input
+	httpIS := services.NewHTTPInputService(bitmovin)
+	httpInput := &models.HTTPInput{
+		Host: stringToPtr("3.88.204.27:10081"),
+	}
+	httpResp, err := httpIS.Create(httpInput)
+	errorHandler(err)
+
+	s3OS := services.NewS3OutputService(bitmovin)
+	s3Output := &models.S3Output{
+		AccessKey:   stringToPtr("AKIAS3ZJEFTTAY4IEM7W"),
+		SecretKey:   stringToPtr("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"),
+		BucketName:  stringToPtr("media.getter-dev.tk"),
+		CloudRegion: bitmovintypes.AWSCloudRegionUSEast1,
+	}
+	s3OutputResp, err := s3OS.Create(s3Output)
+	errorHandler(err)
+
+	encodingS := services.NewEncodingService(bitmovin)
+	encoding := &models.Encoding{
+		Name:        stringToPtr((string)("rdb cloud region test " + cloudRegion)),
+		CloudRegion: cloudRegion,
+	}
+	encodingResp, err := encodingS.Create(encoding)
+	errorHandler(err)
+
+	h264S := services.NewH264CodecConfigurationService(bitmovin)
+	video1080pConfig := &models.H264CodecConfiguration{
+		Name:      stringToPtr("example_video_codec_configuration_1080p"),
+		Bitrate:   intToPtr(4800000),
+		FrameRate: floatToPtr(25.0),
+		Width:     intToPtr(720),
+		Height:    intToPtr(1280),
+		Profile:   bitmovintypes.H264ProfileHigh,
+	}
+	video720Config := &models.H264CodecConfiguration{
+		Name:      stringToPtr("example_video_codec_configuration_720p"),
+		Bitrate:   intToPtr(2400000),
+		FrameRate: floatToPtr(25.0),
+		Width:     intToPtr(480),
+		Height:    intToPtr(854),
+		Profile:   bitmovintypes.H264ProfileHigh,
+	}
+	video1080pResp, err := h264S.Create(video1080pConfig)
+	errorHandler(err)
+	video720Resp, err := h264S.Create(video720Config)
+	errorHandler(err)
+
+	aacS := services.NewAACCodecConfigurationService(bitmovin)
+	aacConfig := &models.AACCodecConfiguration{
+		Name:         stringToPtr("example_audio_codec_configuration"),
+		Bitrate:      intToPtr(128000),
+		SamplingRate: floatToPtr(48000.0),
+	}
+	aacResp, err := aacS.Create(aacConfig)
+	errorHandler(err)
+
+	videoInputStream := models.InputStream{
+		InputID:       httpResp.Data.Result.ID,
+		InputPath:     stringToPtr("rdb_test/HD720x1280_30fps.MOV"),
+		SelectionMode: bitmovintypes.SelectionModeAuto,
+	}
+	audioInputStream := models.InputStream{
+		InputID:       httpResp.Data.Result.ID,
+		InputPath:     stringToPtr("rdb_test/HD720x1280_30fps.MOV"),
+		SelectionMode: bitmovintypes.SelectionModeAuto,
+	}
+
+	vis := []models.InputStream{videoInputStream}
+	videoStream1080p := &models.Stream{
+		CodecConfigurationID: video1080pResp.Data.Result.ID,
+		InputStreams:         vis,
+	}
+	videoStream720p := &models.Stream{
+		CodecConfigurationID: video720Resp.Data.Result.ID,
+		InputStreams:         vis,
+	}
+
+	videoStream1080pResp, err := encodingS.AddStream(*encodingResp.Data.Result.ID, videoStream1080p)
+	errorHandler(err)
+	videoStream720pResp, err := encodingS.AddStream(*encodingResp.Data.Result.ID, videoStream720p)
+	errorHandler(err)
+
+	ais := []models.InputStream{audioInputStream}
+	audioStream := &models.Stream{
+		CodecConfigurationID: aacResp.Data.Result.ID,
+		InputStreams:         ais,
+	}
+	aacStreamResp, err := encodingS.AddStream(*encodingResp.Data.Result.ID, audioStream)
+	errorHandler(err)
+
+	aclEntry := models.ACLItem{
+		Permission: bitmovintypes.ACLPermissionPublicRead,
+	}
+	acl := []models.ACLItem{aclEntry}
+
+	videoMuxingStream1080p := models.StreamItem{
+		StreamID: videoStream1080pResp.Data.Result.ID,
+	}
+	videoMuxingStream720p := models.StreamItem{
+		StreamID: videoStream720pResp.Data.Result.ID,
+	}
+	audioMuxingStream := models.StreamItem{
+		StreamID: aacStreamResp.Data.Result.ID,
+	}
+
+	videoMuxing1080pOutput := models.Output{
+		OutputID:   s3OutputResp.Data.Result.ID,
+		OutputPath: stringToPtr((string)("rdb_test/" + cloudRegion + "/video/1080p")),
+		ACL:        acl,
+	}
+	videoMuxing720pOutput := models.Output{
+		OutputID:   s3OutputResp.Data.Result.ID,
+		OutputPath: stringToPtr((string)("rdb_test/" + cloudRegion + "/video/720p")),
+		ACL:        acl,
+	}
+	audioMuxingOutput := models.Output{
+		OutputID:   s3OutputResp.Data.Result.ID,
+		OutputPath: stringToPtr((string)("rdb_test/" + cloudRegion + "/audio")),
+		ACL:        acl,
+	}
+
+	videoMuxing1080p := &models.TSMuxing{
+		SegmentLength: floatToPtr(4.0),
+		SegmentNaming: stringToPtr("seg_%number%.ts"),
+		Streams:       []models.StreamItem{videoMuxingStream1080p},
+		Outputs:       []models.Output{videoMuxing1080pOutput},
+	}
+	videoMuxing1080pResp, err := encodingS.AddTSMuxing(*encodingResp.Data.Result.ID, videoMuxing1080p)
+	errorHandler(err)
+
+	videoMuxing720p := &models.TSMuxing{
+		SegmentLength: floatToPtr(4.0),
+		SegmentNaming: stringToPtr("seg_%number%.ts"),
+		Streams:       []models.StreamItem{videoMuxingStream720p},
+		Outputs:       []models.Output{videoMuxing720pOutput},
+	}
+	videoMuxing720pResp, err := encodingS.AddTSMuxing(*encodingResp.Data.Result.ID, videoMuxing720p)
+	errorHandler(err)
+
+	audioMuxing := &models.TSMuxing{
+		SegmentLength: floatToPtr(4.0),
+		SegmentNaming: stringToPtr("seg_%number%.ts"),
+		Streams:       []models.StreamItem{audioMuxingStream},
+		Outputs:       []models.Output{audioMuxingOutput},
+	}
+	audioMuxingResp, err := encodingS.AddTSMuxing(*encodingResp.Data.Result.ID, audioMuxing)
+	errorHandler(err)
+
+	_, err = encodingS.Start(*encodingResp.Data.Result.ID)
+	errorHandler(err)
+
+	var status string
+	status = ""
+	for status != "FINISHED" {
+		time.Sleep(10 * time.Second)
+		statusResp, err := encodingS.RetrieveStatus(*encodingResp.Data.Result.ID)
+		if err != nil {
+			fmt.Println("error in Encoding Status")
+			fmt.Println(err)
+			return
+		}
+		// Polling and Printing out the response
+		fmt.Printf("%+v\n", statusResp)
+		status = *statusResp.Data.Result.Status
+		if status == "ERROR" {
+			fmt.Println("error in Encoding Status")
+			fmt.Printf("%+v\n", statusResp)
+			return
+		}
+	}
+
+	manifestOutput := models.Output{
+		OutputID:   s3OutputResp.Data.Result.ID,
+		OutputPath: stringToPtr((string)("rdb_test/" + cloudRegion + "/manifest")),
+		ACL:        acl,
+	}
+	hlsManifest := &models.HLSManifest{
+		ManifestName: stringToPtr("your_manifest_name.m3u8"),
+		Outputs:      []models.Output{manifestOutput},
+	}
+	hlsService := services.NewHLSManifestService(bitmovin)
+	hlsManifestResp, err := hlsService.Create(hlsManifest)
+	errorHandler(err)
+
+	audioMediaInfo := &models.MediaInfo{
+		Type:            bitmovintypes.MediaTypeAudio,
+		URI:             stringToPtr("audio.m3u8"),
+		GroupID:         stringToPtr("audio_group"),
+		Language:        stringToPtr("en"),
+		Name:            stringToPtr("Rendition Description"),
+		IsDefault:       boolToPtr(false),
+		Autoselect:      boolToPtr(false),
+		Forced:          boolToPtr(false),
+		Characteristics: []string{"public.accessibility.describes-video"},
+		SegmentPath:     stringToPtr("../audio"),
+		EncodingID:      encodingResp.Data.Result.ID,
+		StreamID:        aacStreamResp.Data.Result.ID,
+		MuxingID:        audioMuxingResp.Data.Result.ID,
+	}
+	_, err = hlsService.AddMediaInfo(*hlsManifestResp.Data.Result.ID, audioMediaInfo)
+	errorHandler(err)
+
+	video1080pStreamInfo := &models.StreamInfo{
+		Audio:       stringToPtr("audio_group"),
+		SegmentPath: stringToPtr("../video/1080p"),
+		URI:         stringToPtr("video_hi.m3u8"),
+		EncodingID:  encodingResp.Data.Result.ID,
+		StreamID:    videoStream1080pResp.Data.Result.ID,
+		MuxingID:    videoMuxing1080pResp.Data.Result.ID,
+	}
+	_, err = hlsService.AddStreamInfo(*hlsManifestResp.Data.Result.ID, video1080pStreamInfo)
+	errorHandler(err)
+
+	video720pStreamInfo := &models.StreamInfo{
+		Audio:       stringToPtr("audio_group"),
+		SegmentPath: stringToPtr("../video/720p"),
+		URI:         stringToPtr("video_lo.m3u8"),
+		EncodingID:  encodingResp.Data.Result.ID,
+		StreamID:    videoStream720pResp.Data.Result.ID,
+		MuxingID:    videoMuxing720pResp.Data.Result.ID,
+	}
+	_, err = hlsService.AddStreamInfo(*hlsManifestResp.Data.Result.ID, video720pStreamInfo)
+	errorHandler(err)
+
+	_, err = hlsService.Start(*hlsManifestResp.Data.Result.ID)
+	errorHandler(err)
+}
+
+func errorHandler(err error) {
+	if err != nil {
+		switch err.(type) {
+		case models.BitmovinError:
+			fmt.Println("Bitmovin Error")
+		default:
+			fmt.Println("General Error")
+		}
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func stringToPtr(s string) *string {
+	return &s
+}
+
+func intToPtr(i int64) *int64 {
+	return &i
+}
+
+func boolToPtr(b bool) *bool {
+	return &b
+}
+
+func floatToPtr(f float64) *float64 {
+	return &f
+}
